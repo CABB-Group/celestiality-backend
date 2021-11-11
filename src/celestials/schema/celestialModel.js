71abd14367450e2d1b3edb4ba0abf1f92789c507
Angela Dzodzomenyo <enyodzo@gmail.com>
2021-11-10 23:12:24 -0600

    boilerplate, schema, created variables to import CRUD

---
+++ src/celestials/schema/celestialModel.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const mongoose = require('mogoose');
+const { MongoMissingCredentialsError } = require('mongoose/node_modules/mongodb');
+
+const Journal = new mongoose.Schema({
+  name: { type: String, required: true},
+  description: { type: String, required: true},
+  date: { type: String, required: true}
+});
+
+const JournalModel = mongoose.model('Entry', Journal)
+
+module.exports = JournalModel;
--- src/celestials/server.js
+++ src/celestials/server.js
@@ -17,8 +17,28 @@ app.use(express.json());
 app.use(cors());
 
 // MONGO/MONGOOSE CONNECTION
+mongoose.connect(process.env.MONGO_CONNECTION_STRINGS, {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
-
+try{
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console,"connetion error"));
+  db.once('open', () => console.log("succes"));
+} catch {
+  console.log("oops", e.message);
+}
 // ROUTING
+app.listen(PORT, () => console.log(`listening on ${PORT}`)
+);
 
+app.get('/', (req,res) => {
+  res.send("test request received");
+})
 // /journal
+app.get('/journal', getJournal)
+app.post('/journal', postJournal)
+app.delete('/journal/:id', deleteJournal)
+app.put('/journal/:id', updateJournal)
+
+app.get('*', (req,res) => res.status(404).send("we don't understand you"))
